c37d3028e0cf3954bc84cb16de188ca0856de411
Akshay Shukla <arshukla16@gmail.com>
2023-07-02 16:40:06 +0530

    pattern type 6

---
+++ Pattern6.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter the Number: ";
+    cin>>n;
+    
+    int i=1;
+    int count=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<count<<" ";
+            count=count+1;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
