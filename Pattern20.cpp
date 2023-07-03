bc3c34d46045d262edeadb59c615c57a5fc4e0d7
Akshay Shukla <arshukla16@gmail.com>
2023-07-03 19:25:38 +0530

    Pattern type 20

---
+++ Pattern20.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter the Number: ";
+    cin>>n;
+
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n-i+1){
+            cout<<"*";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
