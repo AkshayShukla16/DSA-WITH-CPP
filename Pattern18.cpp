a27ad1046966cad56b401c89b9e0c58e8ce92a43
Akshay Shukla <arshukla16@gmail.com>
2023-07-03 19:24:30 +0530

    Pattern type 18

---
+++ Pattern18.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter the Number: ";
+    cin>>n;
+
+    int i=1;
+    while(i<=n){
+        int j=1;
+        char start ='A'+n-i;
+        while(j<=i){
+            cout<<start<<" ";
+            start++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
