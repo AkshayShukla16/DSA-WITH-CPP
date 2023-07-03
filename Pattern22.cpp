e6b72902b8b97ccbb64e6a07569e67d379dce1ca
Akshay Shukla <arshukla16@gmail.com>
2023-07-03 21:26:52 +0530

    Pattern type 22

---
+++ Pattern22.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter the Number: ";
+    cin>>n;
+
+    int i=1;
+    while(i<=n){
+
+        // print space
+        int space=i-1;
+        while(space>=0){
+            cout<<" ";
+            space= space-1;
+        }
+
+        // print star number
+
+        int j=1;
+        while(j<=n-i+1){
+            cout<<i;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+
+}
+
