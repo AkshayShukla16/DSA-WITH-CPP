c6743b3c3acc63ee820fda8cd718d5584e6b6027
Akshay Shukla <arshukla16@gmail.com>
2023-07-04 21:48:08 +0530

    Bitwise Operator

---
+++ Bitwise_Op.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b;
+    cout<<"Enter the 2 no: ";
+    cin>>a>>b;
+
+    cout<<"a & b is: "<<(a&b)<<endl;
+    cout<<"a | b is: "<<(a|b)<<endl;
+    cout<<"~a is: "<<(~a)<<endl;
+    cout<<"a ^ b is: "<<(a^b)<<endl;
+}
