7bc29226605303253547b66ce4a12ef36afe8fb1
Akshay Shukla <arshukla16@gmail.com>
2023-07-04 21:50:56 +0530

    Left shift & Right Shift

---
+++ Left_Right_Shift.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    cout<<"Enter the no: ";
+    cin>>a;
+
+    cout<<a<<"<<1 is: "<<(a<<1)<<endl;
+    cout<<a<<"<<2 is: "<<(a<<2)<<endl;
+    cout<<a<<">>1 is: "<<(a>>1)<<endl;
+    cout<<a<<">>2 is: "<<(a>>2)<<endl;
+}
