c18aa83920475690f9fd035b9b9ef8f7d7c484a9
Akshay Shukla <arshukla16@gmail.com>
2023-07-04 21:52:01 +0530

    Pre & Post increment

---
+++ Pre_Post-Increment.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a=9;
+   
+   cout<<a++<<endl;   //printed a=9  then a=10 hua
+   cout<<++a<<endl;   // Initially 10 tha ab yaha a=11 hua, a=11
+   cout<<a--<<endl;   // printed a=11, then a=10
+   cout<<--a<<endl;   // a=10 tha ab yaha a=9 hua, a=9 rahega
+}
