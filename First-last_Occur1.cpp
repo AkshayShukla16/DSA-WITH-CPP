98dec51fd6146d61d4baeb9c2b881c39d35c5df0
Akshay Shukla <arshukla16@gmail.com>
2023-07-25 20:53:40 +0530

    first & last occurence of element using Binary Search

---
+++ First-last_Occur1.cpp
@@ -0,0 +1,64 @@
+// here WE HAVE USED BINARY SEARCH METHOD 
+
+#include<iostream>
+using namespace std;
+
+int first_Occur(int arr[], int size, int key){
+    
+    int start=0, end=size;
+    int mid= start+(end-start)/2;     //mid= (start+end)/2; ye bhi use kar sakate hai but problem ho sakta hai
+    int firstIndex= -1;
+
+    while(start<=end){
+     
+        // for leftmosdt occurence (1st occcurence)
+        if(arr[mid]==key){
+            firstIndex= mid;
+            end= mid-1;
+        } 
+        else if(arr[mid]>key){
+            end=mid-1;
+        }
+        else {     // when arr[mid]<key
+            start=mid+1;
+        }
+        mid=  start+(end-start)/2;
+  }
+    return firstIndex;
+}
+
+int last_Occur(int arr[], int size, int key){
+    
+    int start=0, end=size;
+    int mid= start+(end-start)/2;     //mid= (start+end)/2; ye bhi use kar sakate hai but problem ho sakta hai
+    int lastIndex=-1;
+
+    while(start<=end){
+     
+        // for rightmost occurence (last occcurence)
+        if(arr[mid]==key){
+            lastIndex= mid;
+            start= mid+1;
+        } 
+        else if(arr[mid]>key){
+            end=mid-1;
+        }
+        else {     // when arr[mid]<key
+            start=mid+1;
+        }
+        mid=  start+(end-start)/2;
+  }
+    return lastIndex;
+}
+int main(){
+
+    int arr[8]={0,0,1,2,2,2,3,3};
+    int size= sizeof(arr)/sizeof(arr[0]);
+    int key=2;
+    int res1= first_Occur(arr, size, key);
+    int res2=  last_Occur(arr, size, key);
+
+    cout<<"First Occurence of 2 at Index: "<<res1<<endl;
+    cout<<"last Occurence of 2 at Index: "<<res2<<endl;
+   
+}
